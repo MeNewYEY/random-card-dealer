63bc36d5efd41f29080dd8b661b292e9035e9589
yeizmendiz <yahieizmendiz@gmail.com>
2020-10-07 17:44:21 +0000

    random card dealer

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,25 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
+import "../style/index.css";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let suit = getSuit();
+  if (suit == "&#9829") {
+    document.querySelector(".first").style.color = "red";
+    document.querySelector(".last").style.color = "red";
+  }
+  document.querySelector(".first").innerHTML = suit;
+  document.querySelector(".second").innerHTML = getNumber();
+  document.querySelector(".last").innerHTML = suit;
 };
+
+function getNumber() {
+  let number = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let random = Math.floor(Math.random() * number.length);
+  return number[random];
+}
+
+function getSuit() {
+  let suits = ["&#9829", "&#9827", "&#9830"];
+  let random = Math.floor(Math.random() * suits.length);
+  return suits[random];
+}
